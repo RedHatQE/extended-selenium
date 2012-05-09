aa3c78206181f954b1f0de8be99a7d5437404882
Jeff Weiss <jweiss@redhat.com>
2012-05-09 11:37:22 -0400

    moved selenium files back to original location

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject extended-selenium "1.0.2-SNAPSHOT"
+  :description "An extension of the selenium RC client with extra logging and convenience methods"
+  :java-source-path "src"
+  :dependencies [[org.seleniumhq.selenium.client-drivers/selenium-java-client-driver "1.0.2"]
+                 [com.redhat.qe/jul.test.records "1.0.0"]]
+  :javac-options {:debug "on"}
+  :plugins [[lein-eclipse "1.0.0"]])
