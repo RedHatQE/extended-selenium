df04e5eca0703d94938783f6b36f6d885e0aca58
Jeff Weiss <jweiss@redhat.com>
2012-05-17 15:09:11 -0400

    Fix NPE that I accidentally introduced with last commit

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.redhat.qe/extended-selenium "1.0.3.1"
+(defproject com.redhat.qe/extended-selenium "1.0.3.2"
   :description "An extension of the selenium RC client with extra logging and convenience methods"
   :java-source-path "src"
   :dependencies [[org.seleniumhq.selenium.client-drivers/selenium-java-client-driver "1.0.2"]
